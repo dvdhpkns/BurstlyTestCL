ee4316b48ee1a7394f04f785ead43cd4ba237910
David Hopkins <dhopkins@burstly.com>
2012-08-28 13:00:47 -0700

    Banner page complete with button refresh and timer

--- src/com/burstly/cltestapp/BannerFragment.java
+++ src/com/burstly/cltestapp/BannerFragment.java
@@ -5,9 +5,13 @@ package com.burstly.cltestapp;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v4.app.Fragment;
 import android.util.Log;
-import android.view.*;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import com.burstly.conveniencelayer.Burstly;
@@ -25,6 +29,7 @@ public class BannerFragment extends Fragment {
     private static final String TAG = "BannerFragment";
     private static final int SWIPE_MIN_DISTANCE = 40;
     private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+    private static final int REFRESH_TIME = 20;
 
     @Override
     public void onResume() {
@@ -69,7 +74,10 @@ public class BannerFragment extends Fragment {
         ViewGroup layout = (ViewGroup)inflater.inflate(R.layout.banner, container, false);
         LinearLayout parentLayout = (LinearLayout) layout.findViewById(R.id.bannerParentLayout);
 
+
+        /*
         final TextView textView = (TextView) layout.findViewById(R.id.textView);
+
         final GestureDetector myGestDetector = new GestureDetector(this.getActivity(), new GestureDetector.SimpleOnGestureListener() {
             @Override
             public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
@@ -99,7 +107,7 @@ public class BannerFragment extends Fragment {
                 myGestDetector.onTouchEvent(event);
                 return true;
             }
-        });
+        }); */
 
         for(AdNetworks ad : AdNetworks.values()){
             String zone = ad.getBannerZone();
@@ -110,6 +118,7 @@ public class BannerFragment extends Fragment {
             LayoutInflater vi = (LayoutInflater) this.getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             bannerView = vi.inflate(R.layout.banner_inflater, null);
 
+
             //set adName in TextView
             TextView bannerAdName = (TextView)bannerView.findViewById(R.id.bannerAdName);
             bannerAdName.setText(adName);
@@ -117,37 +126,49 @@ public class BannerFragment extends Fragment {
             final TextView status = (TextView)bannerView.findViewById(R.id.adStatus);
             status.setText("Loading Banner...");
 
+            LinearLayout progressBar = (LinearLayout)bannerView.findViewById(R.id.progressBar);
+            ProgressBar pB = new ProgressBar(REFRESH_TIME*1000, 10, progressBar);
+
             LinearLayout bannerParent = (LinearLayout)bannerView.findViewById(R.id.bannerParent);
 
             //Add BurstlyBanner to layout
-            BurstlyBanner banner = new BurstlyBanner(this.getActivity(),
+            final BurstlyBanner banner = new BurstlyBanner(this.getActivity(),
                     bannerParent,
                     new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT),
                     ad.appId,
                     zone,
                     adName,
-                    20);
-            banner.addBurstlyListener(getBurstlyListener(status));
+                    REFRESH_TIME);
+            banner.addBurstlyListener(getBurstlyListener(status, pB));
             banner.showAd();
 
 
 
+            ImageView refreshButton = (ImageView)bannerView.findViewById(R.id.refreshButton);
+            refreshButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    banner.showAd();
+                }
+            });
 
             parentLayout.addView(bannerView);
         }
         return layout;
     }
 
-    private BurstlyListenerAdapter getBurstlyListener(final TextView status) {
+    private BurstlyListenerAdapter getBurstlyListener(final TextView status, final ProgressBar pB) {
         BurstlyListenerAdapter listener = new BurstlyListenerAdapter() {
             @Override
             public void onShow(final BurstlyBaseAd ad, final AdShowEvent event) {
                 status.setText("Ad Shown");
+                pB.start();
                 Log.d(TAG, "Ad has been shown: " + ad.getName());
             }
 
             @Override
             public void onCache(final BurstlyBaseAd ad, final AdCacheEvent event) {
+                //should not be seen
                 status.setText("Ad Cached");
             }
 
@@ -157,10 +178,42 @@ public class BannerFragment extends Fragment {
                 if(event.wasRequestThrottled())
                     status.setText("Throttled");
                 else
-                    status.setText("onFail: " + event.toString());
+                    status.setText("Failed");
+                    Log.d(TAG, "onFail: " + event.toString());
             }
         };
 
         return listener;
+    }
+
+    class ProgressBar extends CountDownTimer {
+        private LinearLayout progressBar;
+        private View timeDown;
+        private View timeRemaining;
+        private long millisInFuture;
+
+        public ProgressBar(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        public ProgressBar(long millisInFuture, long countDownInterval, LinearLayout progressBar) {
+            super(millisInFuture, countDownInterval);
+            this.millisInFuture= millisInFuture;
+            this.progressBar = progressBar;
+            timeDown = (View)progressBar.findViewById(R.id.timeDown);
+            timeRemaining = (View)progressBar.findViewById(R.id.timeRemaining);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            timeDown.setLayoutParams(new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.MATCH_PARENT, (millisInFuture-millisUntilFinished)));
+            timeRemaining.setLayoutParams(new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.MATCH_PARENT, (millisUntilFinished)));
+        }
+
+        @Override
+        public void onFinish() {
+            timeDown.setLayoutParams(new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.MATCH_PARENT, 1));
+            timeRemaining.setLayoutParams(new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.MATCH_PARENT, 0));
+        }
     }
 }
